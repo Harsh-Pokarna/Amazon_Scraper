fea82e01469d10fbcda92ad083e1b4af47246431
Harsh <f20200754@pilani.bits-pilani.ac.in>
2021-01-18 09:23:49 +0530

    my changes

--- app/src/main/java/com/example/practise/MainActivity.java
+++ app/src/main/java/com/example/practise/MainActivity.java
@@ -8,8 +8,11 @@ import android.provider.DocumentsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -21,38 +24,43 @@ import java.lang.ref.WeakReference;
 import java.net.URL;
 import java.util.NoSuchElementException;
 
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
+
 public class MainActivity extends AppCompatActivity {
-     String value;
+     String value, url, xyz;
      TextView tv;
-     String url;
-     String object;
      EditText editText;
      Button search;
-
+     ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.textView);
+        tv.setHint("Image will be displayed here");
         editText = findViewById(R.id.edit_text);
-        object = editText.getText().toString();
-        url = "https://www.amazon.com/s?k=" + object;
+        imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.welcome);
         search = findViewById(R.id.search_Btn);
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new scratch(tv, value, url).execute();
-    }
+        search.setOnClickListener(v -> {
+            xyz = editText.getText().toString();
+            System.out.println(xyz);
+            url = "https://www.amazon.in/s?k=" + xyz + "&crid=24WYPNHZGYNIJ&sprefix=keyb%2Caps%2C282&ref=nb_sb_ss_ts-a-p_7_4";
+            new scratch(tv, value, url).execute();
 });
 
         }
+
     public  class scratch extends AsyncTask<String, String , String > {
         String value;
         TextView tv;
         String url;
         Elements allinfo;
         Document doc;
+
 
         scratch(TextView tv, String value, String url){
             this.tv = tv;
@@ -64,21 +72,26 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             tv.setText(value);
             Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+
         }
 
         @Override
         protected String doInBackground(String... strings) {
             try {
-               doc = Jsoup.connect(url).get();
+              doc = Jsoup.connect(url).get();
+                allinfo = doc.select("div[class=\"a-section aok-relative s-image-fixed-height\"]");
+                Elements tags = allinfo.select("img[class=\"s-image\"]");
+                String name = tags.attr("alt");
+                name = name.replace("Sponsored Ad - ", "");
+                value= name;
+                String link = tags.attr("src");
+                runOnUiThread(() -> Glide.with(MainActivity.this).load(link).fitCenter().into(imageView));
+
             } catch (IOException e) {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
-            allinfo = doc.getElementsByClass("a-section aok-relative s-image-fixed-height");
-            for (Element link : allinfo){
-                Elements tags = link.getElementsByTag("img");
-                System.out.println(tags.attr("src"));
 
-            }
 
 
             return null;
